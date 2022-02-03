723db06b5c573ca847938f7949fb6b014a60921b
Harumi Hokari <hhokari@unix1.csc.calpoly.edu>
2022-02-03 01:48:37 -0800

    added more test cases and added comments for easier comprehension

--- func.py
+++ func.py
@@ -3,37 +3,52 @@ import csv
 import getopt
 from datetime import datetime
 import time
+import logging
 
 same_date_list = []
 
 argumentList = sys.argv
 
+# check for sufficient number of arguments
 if len(argumentList) <= 3:
-    print("Not enough arguments")
-    exit(1)
+    raise TypeError("wrong number of arguments")
 
 try:
     arguments, values = getopt.getopt(argumentList[2:], "d:")
     
     for cur_arg, cur_val in arguments:
+        # if d parameter found, input_day is set to parameter argument (date)
         if cur_arg in ['-d']:
             input_day = cur_val
             
     format  = '%Y-%m-%dT%H:%M:%S'
 
+    # key: cookie, val: total number of seconds
     time_dict = {}
 
-    with open(sys.argv[1], 'r') as csv_file:
-        read = csv.reader(csv_file)
-        
-        next(read)
-        for row in read:
-            converted_datetime = datetime.strptime(row[1][:19], format)
-            total_time = time.mktime(converted_datetime.timetuple())
-            if row[1][:10] == input_day:
-                if row[0] not in time_dict.keys():
-                    time_dict[row[0]] = total_time
-                same_date_list.append(row[0])
+    try:
+        with open(sys.argv[1], 'r') as csv_file:
+            read = csv.reader(csv_file)
+
+            # skip header of csv file
+            next(read)
+            for row in read:
+                converted_datetime = datetime.strptime(row[1][:19], format)
+                # convert to total number of seconds for easier comparison for later
+                total_time = time.mktime(converted_datetime.timetuple())
+                if row[1][:10] == input_day:
+                    if row[0] not in time_dict.keys():
+                        time_dict[row[0]] = total_time
+                    same_date_list.append(row[0])
+
+            # list being empty implies, invalid date provided       
+            if len(same_date_list) == 0:
+                raise ValueError("inputted date does not exist")
+
+    # invalid file provided
+    except IOError as e:
+        logging.exception(e)
+        exit(1)
 
 
 except getopt.error as err:
@@ -43,6 +58,7 @@ except getopt.error as err:
 
 cookie_dict = {}
 
+# determine frequencies of cookies
 for cookie in same_date_list:
     if cookie not in cookie_dict.keys():
         cookie_dict[cookie] = 1
@@ -50,9 +66,13 @@ for cookie in same_date_list:
         cookie_dict[cookie] += 1
 
 
-
+# get max frequency
 max_freq = max(cookie_dict.values())
+
+# max_list = [(cookie, total number of seconds)]
 max_list = [(key,time_dict[key]) for key, freq in cookie_dict.items() if freq == max_freq]
+
+# sort by total number of seconds, reversed for descending order
 max_list = sorted(max_list, key=lambda x: x[1])[::-1]
 for cookie in max_list:
     print(cookie[0])
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import subprocess
+import os
 
 class TestActiveCookie(unittest.TestCase):
 
@@ -11,6 +12,30 @@ class TestActiveCookie(unittest.TestCase):
 
      def test_recent_cookie_3(self):
          self.assertEqual(subprocess.check_output("python func.py 'most_active_cookie cookie_log.csv' -d 2018-12-07", shell=True, universal_newlines=True), '4sMM2LxV07bPJzwf\n')
+
+     def test_not_enough_arguments_1(self):
+         try:
+            subprocess.check_output("python func.py", shell=True, universal_newlines=True)
+         except subprocess.CalledProcessError as e:
+            self.assertEqual(e.returncode, 1)
+
+     def test_not_enough_arguments_2(self):
+         try:
+            subprocess.check_output("python func.py 'most_active_cookie cookie_log.csv'", shell=True, universal_newlines=True)
+         except subprocess.CalledProcessError as e:
+            self.assertEqual(e.returncode, 1)       
+
+     def test_not_enough_arguments_3(self):
+         try:
+            subprocess.check_output("python func.py 'most_active_cookie cookie_log.csv' -d", shell=True, universal_newlines=True)
+         except subprocess.CalledProcessError as e:
+            self.assertEqual(e.returncode, 1)
+
+     def test_invalid_file(self):
+         try:
+            subprocess.check_output("python func.py 'non_existent_file.csv' -d", shell=True, universal_newlines=True)
+         except subprocess.CalledProcessError as e:
+            self.assertEqual(e.returncode, 1)
 
 if __name__ == '__main__':
     unittest.main()
